b1e267379ee9b44cf09fff26a8641e9d68601510
GiaMadonna1 <109568865+GiaMadonna1@users.noreply.github.com>
2023-04-18 12:45:49 -0700

    Got everything working except new fact page

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -4,7 +4,12 @@ def create_app():
     app = Flask(__name__)
 
     @app.route('/')
-    def hello():
+    def index():
         return 'Hello, PetFax!'
     
+    
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+
     return app 
